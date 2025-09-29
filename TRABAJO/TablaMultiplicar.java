4a3b5d9e9f72f882aeb00fa42b6623a0bc4a166f
Jps-06 <jstivenpena@estudiante.uniajc.edu.co>
2025-09-29 20:06:25 +0000

    codigo terminado

---
+++ TRABAJO/TablaMultiplicar.java
@@ -0,0 +1,19 @@
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        // Imprimir la cabecera
+        System.out.print("    "); // Espacio para alineación
+        for (int j = 1; j <= 15; j++) {
+            System.out.printf("%4d", j);
+        }
+        System.out.println();
+ 
+        // Imprimir las filas de la tabla
+        for (int i = 1; i <= 15; i++) {
+            System.out.printf("%2d* ", i);
+            for (int j = 1; j <= 15; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
